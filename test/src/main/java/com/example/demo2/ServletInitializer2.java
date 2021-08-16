f607431e0672597d1c4e98b0ed3a334488f2e0e1
lee <lee@LAPTOP-P9Q0E3JQ>
2021-08-17 01:07:19 +0900

    project

--- test/src/main/java/com/example/demo2/ServletInitializer2.java
+++ test/src/main/java/com/example/demo2/ServletInitializer2.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example.demo2;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer2 extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
